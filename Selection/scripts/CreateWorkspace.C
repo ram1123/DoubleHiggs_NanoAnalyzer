ca50056ce32143f2b3ad38ae75ff831bf3586db2
ram1123 <ramkrishna.sharma71@gmail.com>
2020-10-07 05:06:26 -0500

    Updated workspace creator; now the workspace looks same as flashgg

--- Selection/scripts/CreateWorkspace.C
+++ Selection/scripts/CreateWorkspace.C
@@ -17,33 +17,33 @@
   int entries = myTree->GetEntriesFast();
   std::cout <<"Total entries: " << entries << std::endl;
 
-  RooRealVar* CMS_hgg_mass = new RooRealVar("CMS_hgg_mass","CMS_hgg_mass",125.6,100,180, "GeV");
-  RooRealVar* weight = new RooRealVar("weight","weight",-10,10);
+  RooRealVar CMS_hgg_mass("CMS_hgg_mass", "CMS_hgg_mass", 125.10,100,180, "GeV");
+  RooRealVar weight("weight", "weight", -10,10);
 
-  RooArgSet* ArgSet = new RooArgSet("args");
-  ArgSet->add(*CMS_hgg_mass);
-  ArgSet->add(*weight);
-  RooDataSet* ggh_125_13TeV_cat0 = new RooDataSet("ggh_125_13TeV_cat0", "ggh_125_13TeV_cat0", *ArgSet, "weight");
+  RooDataSet ggh_125_13TeV_cat0("ggh_125_13TeV_cat0", "ggh_125_13TeV_cat0", RooArgSet(CMS_hgg_mass,weight));
 
-  // Fill all integers from 0 to 100 as mass
   for (int i=0; i < entries; i++)
   {
     int nb = myTree->GetEntry(i);
     if (diphoton_m < 0.) continue;
-    CMS_hgg_mass->setVal(diphoton_m);
-    weight->setVal(xsec*Lumi*genWeight/(1.0*nTotal));
-    ggh_125_13TeV_cat0->add(*ArgSet,xsec*Lumi*genWeight/(1.0*nTotal));
+    CMS_hgg_mass.setVal(diphoton_m);
+    weight.setVal(xsec*Lumi*genWeight/(1.0*nTotal));
+    ggh_125_13TeV_cat0.add(RooArgSet(CMS_hgg_mass,weight));
   }
 
-  TCanvas* c1 = new TCanvas("c1", "After applying weight");
-  RooPlot* massDist = CMS_hgg_mass->frame(RooFit::Title("CMS_hgg_mass"));
-  ggh_125_13TeV_cat0->plotOn(massDist);
-  massDist->Draw();
-  c1->SaveAs("AfterWeight.png");
+  ggh_125_13TeV_cat0.Print("v");
 
-  RooWorkspace workspace("CMS_hgg_13TeV", "CMS_hgg_13TeV");
-  workspace.import(*ggh_125_13TeV_cat0);
-  workspace.import(*weight);
-  workspace.writeToFile("workspace.root");
+  RooWorkspace *workspace = new RooWorkspace("CMS_hgg_13TeV", "CMS_hgg_13TeV");
+  workspace->import(ggh_125_13TeV_cat0);
+  workspace->Print();
+
+  // TCanvas* c1 = new TCanvas("c1", "After applying weight");
+  // RooPlot* massDist = CMS_hgg_mass->frame(RooFit::Title("CMS_hgg_mass"));
+  // ggh_125_13TeV_cat0->plotOn(massDist);
+  // massDist->Draw();
+  // c1->SaveAs("AfterWeight.png");
+
+  workspace->writeToFile("workspace.root","kTRUE");
+
 }
 
