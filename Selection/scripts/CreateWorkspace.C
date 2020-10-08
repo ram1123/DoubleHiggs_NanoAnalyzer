602ed4ca38661426bde9e20e35277eebd68064d7
ram1123 <ramkrishna.sharma71@gmail.com>
2020-10-08 02:36:06 -0500

    Update workspace creator

--- Selection/scripts/CreateWorkspace.C
+++ Selection/scripts/CreateWorkspace.C
@@ -1,49 +1,62 @@
 {
-  TFile * myFile = new TFile("/uscms/home/rasharma/nobackup/double-higgs/nanoAODAnalyzer/CMSSW_10_2_22/src/DoubleHiggs/Selection/output.root","r");
+  TFile * myFile = new TFile("/uscms/home/rasharma/nobackup/double-higgs/nanoAODAnalyzer/CMSSW_10_2_22/src/DoubleHiggs/Selection/Signal.root","r");
   TTree *myTree = (TTree*)myFile->Get("Events");
 
-  float diphoton_m, genWeight;
+  float diphoton_m, genWeight,diphoton_pt;
   myTree->SetBranchAddress("diphoton_m",&diphoton_m);
   myTree->SetBranchAddress("genWeight",&genWeight);
+  myTree->SetBranchAddress("diphoton_pt",&diphoton_pt);
 
   double nTotal=1, nNeg=1;
   TH1F* hTotEvents = (TH1F*) myFile->Get("TotalEvents"); assert(hTotEvents);
   nTotal = hTotEvents->GetBinContent(2);
   nNeg = hTotEvents->GetBinContent(1);
 
-  float Lumi = 3400.0;
+  float Lumi = 41.5;
   float xsec = 0.001;
 
   int entries = myTree->GetEntriesFast();
   std::cout <<"Total entries: " << entries << std::endl;
 
-  RooRealVar CMS_hgg_mass("CMS_hgg_mass", "Mass of Higgs boson decaying to photons", 125.10,100,180, "GeV");
-  RooRealVar weight("weight", "weight", -10,10);
+  RooRealVar CMS_hgg_mass("CMS_hgg_mass", "CMS_hgg_mass", 125.10,100,180, "GeV");
+  RooRealVar IntLumi("IntLumi", "IntLumi", 36.5);
+  RooRealVar weight("weight", "weight", -1000,1000);
 
-  RooDataSet ggh_125_13TeV_cat0("ggh_125_13TeV_cat0", "ggh_125_13TeV_cat0", RooArgSet(CMS_hgg_mass,weight));
+  RooDataSet Data_13TeV_HHWWggTag_2("GluGluToHHTo_WWgg_qqqq_nodeSM_13TeV_HHWWggTag_2", "GluGluToHHTo_WWgg_qqqq_nodeSM_13TeV_HHWWggTag_2", RooArgSet(CMS_hgg_mass));
+  RooDataSet Data_13TeV_HHWWggTag_2_weight("GluGluToHHTo_WWgg_qqqq_nodeSM_13TeV_HHWWggTag_2_weight", "GluGluToHHTo_WWgg_qqqq_nodeSM_13TeV_HHWWggTag_2_weight", RooArgSet(CMS_hgg_mass,weight));
 
   for (int i=0; i < entries; i++)
   {
     int nb = myTree->GetEntry(i);
-    if (diphoton_m < 0.) continue;
+    // if (diphoton_m < 0.) continue;
+    if (diphoton_pt<100.0) continue;
+    if (diphoton_m < 100. || diphoton_m > 180) continue;
     CMS_hgg_mass.setVal(diphoton_m);
     weight.setVal(xsec*Lumi*genWeight/(1.0*nTotal));
-    ggh_125_13TeV_cat0.add(RooArgSet(CMS_hgg_mass,weight));
+    // Data_13TeV_HHWWggTag_2.add(RooArgSet(CMS_hgg_mass));
+    Data_13TeV_HHWWggTag_2.add(RooArgSet(CMS_hgg_mass,weight));
+    Data_13TeV_HHWWggTag_2_weight.add(RooArgSet(CMS_hgg_mass,weight),xsec*Lumi*genWeight/(1.0*nTotal));
   }
 
-  ggh_125_13TeV_cat0.Print("v");
+  Data_13TeV_HHWWggTag_2.Print("v");
 
-  RooWorkspace *workspace = new RooWorkspace("CMS_hgg_13TeV", "CMS_hgg_13TeV");
-  workspace->import(ggh_125_13TeV_cat0);
+  RooWorkspace *workspace = new RooWorkspace("cms_hgg_13TeV", "cms_hgg_13TeV");
+  workspace->import(Data_13TeV_HHWWggTag_2);
   workspace->Print();
 
-  // TCanvas* c1 = new TCanvas("c1", "After applying weight");
-  // RooPlot* massDist = CMS_hgg_mass->frame(RooFit::Title("CMS_hgg_mass"));
-  // ggh_125_13TeV_cat0->plotOn(massDist);
-  // massDist->Draw();
-  // c1->SaveAs("AfterWeight.png");
+  TCanvas* c1 = new TCanvas("c1", "Without weight");
+  RooPlot* massDist = CMS_hgg_mass.frame(RooFit::Title("CMS_hgg_mass"));
+  Data_13TeV_HHWWggTag_2.plotOn(massDist);
+  massDist->Draw();
+  c1->SaveAs("NoWeight.png");
 
-  workspace->writeToFile("workspace.root","kTRUE");
+  TCanvas* c2 = new TCanvas("c2", "After applying weight");
+  RooPlot* massDist2 = CMS_hgg_mass.frame(RooFit::Title("CMS_hgg_mass"));
+  Data_13TeV_HHWWggTag_2_weight.plotOn(massDist2);
+  massDist2->Draw();
+  c2->SaveAs("AfterWeight.png");
+
+  workspace->writeToFile("nodeSM_HHWWgg_qqqq.root","kTRUE");
 
 }
 
