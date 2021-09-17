fba8360ea6710b421959b244ba45a9e12c997d40
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-09-17 22:34:30 +0200

    Updated path of input sample text files

--- Selection/GetTxtFileFromDAS.py
+++ Selection/GetTxtFileFromDAS.py
@@ -3,6 +3,7 @@ import os
 
 InputFileFromWhereReadDASNames = '/afs/cern.ch/user/r/rasharma/work/doubleHiggs/ResonantAnalysis/nanoAODSetup/CMSSW_10_6_20/src/PhysicsTools/NanoAODTools/python/postprocessing/analysis/nanoAOD_vvVBS/input_data_Files/HH_sample_list_v7_2017_campaign.txt'
 
+ifData = 1
 with open(InputFileFromWhereReadDASNames) as in_file:
     for lines in in_file:
         if not lines.startswith("#"):
@@ -11,7 +12,10 @@ with open(InputFileFromWhereReadDASNames) as in_file:
             campaign = lines.split('/')[2].split('-')[0]
             print "==> sample_name = ", sample_name
             print "==> campaign = ", campaign
-            outjdl_file = open(sample_name + ".txt", "w")
+
+            if ifData: outjdl_file = open(sample_name + "_" + campaign + ".txt", "w")
+            else: outjdl_file = open(sample_name + ".txt", "w")
+
             xrd_redirector = 'root://cms-xrd-global.cern.ch/'
             output = os.popen('dasgoclient --query="file dataset=' + lines.strip() + '"').read()
             for root_file in output.split():
