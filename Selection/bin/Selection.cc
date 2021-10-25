2461ab97fcd9a7c5b3bf3b82ae947ab1e64d1e1e
Zang Zhenxuan <1157911514@qq.com>
2021-10-25 15:10:16 +0200

    photon selection

--- Selection/bin/Selection.cc
+++ Selection/bin/Selection.cc
@@ -740,7 +740,7 @@ int main (int argc, char** argv) {
                     WVJJTree->pho1_mvaID_WP80 = NanoReader_.Photon_mvaID_WP80[PhotonCount];
                     WVJJTree->pho1_mvaID_WP90 = NanoReader_.Photon_mvaID_WP90[PhotonCount];
                 }
-                else if ( WVJJTree->pho1_pt > NanoReader_.Photon_pt[PhotonCount] > WVJJTree->pho2_pt ) {
+                else if ( (NanoReader_.Photon_pt[PhotonCount] > WVJJTree->pho2_pt) && ( WVJJTree->pho1_pt >NanoReader_.Photon_pt[PhotonCount]) ) {
                     WVJJTree->pho2_pt = NanoReader_.Photon_pt[PhotonCount];
                     WVJJTree->pho2_eta = NanoReader_.Photon_eta[PhotonCount];
                     WVJJTree->pho2_phi = NanoReader_.Photon_phi[PhotonCount];
@@ -761,9 +761,9 @@ int main (int argc, char** argv) {
                 }
             /* ----------------- Leading and SubLeading photon selection ---------------- */
 
-            if (!(nTightPhoton==2)) continue;
             if(!(WVJJTree->pho1_pt > PHO1_PT_CUT)) continue;
             if(!(WVJJTree->pho2_pt > PHO2_PT_CUT)) continue;
+            if (!(nTightPhoton >= 2)) continue;
             totalCutFlow_SL->Fill("Photon Selection",1);
             totalCutFlow_SL_GENMatch->Fill("Photon Selection",1);
 
